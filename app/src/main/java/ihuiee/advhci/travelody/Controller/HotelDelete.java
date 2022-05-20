568bc5015a784082798439b5673c398d060ef208
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2022-05-20 16:51:48 +0300

    feature/Delete Hotel ready

--- app/src/main/java/ihuiee/advhci/travelody/Controller/HotelDelete.java
+++ app/src/main/java/ihuiee/advhci/travelody/Controller/HotelDelete.java
@@ -2,59 +2,45 @@ package ihuiee.advhci.travelody.Controller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.room.Room;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ihuiee.advhci.travelody.DB.AppDatabase;
+import ihuiee.advhci.travelody.DB.Cities;
+import ihuiee.advhci.travelody.DB.Hotels;
 import ihuiee.advhci.travelody.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HotelDelete#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HotelDelete extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button hotelDeleteBtn;
+    String selectedCountry;
+    String selectedCity;
+    String selectedHotel;
+    List<String> countriesList = new ArrayList<>();
+    List<String> citiesList = new ArrayList<>();
+    List<String> hotelList = new ArrayList<>();
+    String defaultSelection = "CHOOSE";
+    Spinner country;
+    Spinner city;
+    Spinner hotel;
 
     public HotelDelete() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HotelDelete.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HotelDelete newInstance(String param1, String param2) {
-        HotelDelete fragment = new HotelDelete();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +48,128 @@ public class HotelDelete extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hotel_delete, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        hotelDeleteBtn = view.findViewById(R.id.HotelDeleteButton);
+        AppDatabase db = Room.databaseBuilder(requireContext(), AppDatabase.class, "TravelodyDB").allowMainThreadQueries().build();
+        country = view.findViewById(R.id.HotelDeleteChooseCountry);
+        ArrayAdapter countryAdapter = new ArrayAdapter<String>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new ArrayList<>());
+        city = view.findViewById(R.id.HotelDeleteChooseCity);
+        ArrayAdapter cityAdapter = new ArrayAdapter<String>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new ArrayList<>());
+        hotel = view.findViewById(R.id.HotelDeleteChoose);
+        ArrayAdapter hotelAdapter = new ArrayAdapter<String>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new ArrayList<>());
+
+        cityAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        city.setAdapter(cityAdapter);
+
+        hotelAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        hotel.setAdapter(hotelAdapter);
+
+        countriesList.addAll(db.countriesDao().getCountryNames());
+        countriesList.sort(String::compareToIgnoreCase);
+        countriesList.add(0, defaultSelection);
+
+        countryAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        country.setAdapter(countryAdapter);
+        countryAdapter.addAll(countriesList);
+        countryAdapter.notifyDataSetChanged();
+        country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = parent.getItemAtPosition(position).toString();
+                citiesList.clear();
+                citiesList.addAll(db.citiesDao().getCitiesOfCountry(db.countriesDao().getCountryIdByName(selectedCountry)));
+
+                hotelList.clear();
+                hotelList.add(0, defaultSelection);
+                hotelAdapter.clear();
+                hotelAdapter.addAll(hotelList);
+                hotel.setSelection(0);
+
+                citiesList.sort(String::compareToIgnoreCase);
+                citiesList.add(0, defaultSelection);
+                cityAdapter.clear();
+                cityAdapter.addAll(citiesList);
+                cityAdapter.notifyDataSetChanged();
+                city.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+//        System.out.println(citiesList);
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCity = parent.getItemAtPosition(position).toString();
+                hotelList.clear();
+                hotelList.addAll(db.hotelsDao().getHotelsOfCity(db.citiesDao().getCitiesIdByName(selectedCity)));
+
+                hotelList.sort(String::compareToIgnoreCase);
+                hotelList.add(0, defaultSelection);
+                hotelAdapter.clear();
+                hotelAdapter.addAll(hotelList);
+                hotelAdapter.notifyDataSetChanged();
+                hotel.setSelection(0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        hotel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedHotel = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        hotelDeleteBtn.setOnClickListener(view1 -> {
+            if (!selectedCity.equals(defaultSelection) && !selectedCountry.equals(defaultSelection) && !selectedHotel.equals(defaultSelection)) {
+                try {
+                    Hotels hotelObj = db.hotelsDao().getHotel(selectedHotel, db.citiesDao().getCitiesIdByName(selectedCity), db.countriesDao().getCountryIdByName(selectedCountry));
+                    db.hotelsDao().deleteHotel(hotelObj);
+                    Toast.makeText(requireActivity().getApplicationContext(), "Hotel Deleted", Toast.LENGTH_LONG).show();
+
+                    cityAdapter.clear();
+                    citiesList.clear();
+                    hotelAdapter.clear();
+                    hotelList.clear();
+                    countryAdapter.clear();
+                    countriesList.clear();
+                    countriesList.addAll(db.countriesDao().getCountryNames());
+                    countriesList.sort(String::compareToIgnoreCase);
+                    countriesList.add(0, defaultSelection);
+                    countryAdapter.addAll(countriesList);
+                    countryAdapter.notifyDataSetChanged();
+                    country.setSelection(0);
+                    citiesList.add(0, defaultSelection);
+                    cityAdapter.addAll(citiesList);
+                    city.setSelection(0);
+                    hotelList.add(0, defaultSelection);
+                    hotelAdapter.addAll(hotelList);
+                    hotel.setSelection(0);
+                } catch (Exception e) {
+                    Toast.makeText(requireActivity().getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+            } else
+                Toast.makeText(requireActivity().getApplicationContext(), "All fields are Required", Toast.LENGTH_LONG).show();
+
+
+        });
     }
 }
--- app/src/main/java/ihuiee/advhci/travelody/DB/HotelsDAO.java
+++ app/src/main/java/ihuiee/advhci/travelody/DB/HotelsDAO.java
@@ -29,4 +29,7 @@ public interface HotelsDAO {
 
     @Query("SELECT hotel_id FROM hotels WHERE hotel_name = :name ")
     int getHotelIdByHotelName(String name);
+
+    @Query("SELECT * FROM hotels WHERE hotel_name = :hotelName AND hotel_city_id = :city_id AND hotel_country_id = :country_id ")
+    Hotels getHotel(String hotelName, int city_id, int country_id);
 }
