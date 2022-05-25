177a242ba5fdbf4e4c9a0f296273db9a1b91099c
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2022-05-25 18:27:41 +0300

    feature/Update City ready

--- app/src/main/java/ihuiee/advhci/travelody/Controller/CityUpdate.java
+++ app/src/main/java/ihuiee/advhci/travelody/Controller/CityUpdate.java
@@ -2,65 +2,127 @@ package ihuiee.advhci.travelody.Controller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.room.Room;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ihuiee.advhci.travelody.DB.AppDatabase;
+import ihuiee.advhci.travelody.DB.Cities;
+import ihuiee.advhci.travelody.DB.Hotels;
 import ihuiee.advhci.travelody.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CityUpdate#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CityUpdate extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CityUpdate() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CityUpdate.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CityUpdate newInstance(String param1, String param2) {
-        CityUpdate fragment = new CityUpdate();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    List<String> countriesList = new ArrayList<>();
+    List<String> citiesList = new ArrayList<>();
+    String selectedCountry;
+    String selectedCity;
+    String defaultSelection = "CHOOSE";
+    Button submitButton;
+    EditText cityName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_city_update, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AppDatabase db = Room.databaseBuilder(requireContext(), AppDatabase.class, "TravelodyDB").allowMainThreadQueries().build();
+        Spinner country=view.findViewById(R.id.ChooseCountryCityUpdate);
+        ArrayAdapter countryAdapter = new ArrayAdapter<String>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new ArrayList<>());
+        Spinner city=view.findViewById(R.id.CityUpdateChoose);
+        ArrayAdapter cityAdapter = new ArrayAdapter<String>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new ArrayList<>());
+        submitButton=view.findViewById(R.id.CityUpdateButton);
+        cityName=view.findViewById(R.id.CityUpdateNameTextPlain);
+
+        cityAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        city.setAdapter(cityAdapter);
+
+
+        countriesList.addAll(db.countriesDao().getCountryNames());
+        countriesList.sort(String::compareToIgnoreCase);
+        countriesList.add(0,defaultSelection);
+
+        citiesList.addAll(db.citiesDao().getCityNames());
+        citiesList.sort(String::compareToIgnoreCase);
+        citiesList.add(0,defaultSelection);
+        cityAdapter.addAll(citiesList);
+        cityAdapter.notifyDataSetChanged();
+
+        countryAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        country.setAdapter(countryAdapter);
+        countryAdapter.addAll(countriesList);
+        countryAdapter.notifyDataSetChanged();
+        country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedCity = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
+
+        submitButton.setOnClickListener(view1 -> {
+            if (!selectedCity.equals(defaultSelection) && !TextUtils.isEmpty(cityName.getText().toString())){
+                Cities city1 = new Cities();
+                city1.setNameOfCity(cityName.getText().toString());
+                city1.setIdOfCity(db.citiesDao().getCitiesIdByName(selectedCity));
+                if(selectedCountry.equals(defaultSelection)) {
+                    city1.setIdOfCountry(db.citiesDao().getCityById(db.citiesDao().getCitiesIdByName(selectedCity)).idOfCountry);
+                }else city1.setIdOfCountry(db.countriesDao().getCountryIdByName(selectedCountry));
+                try {
+                    db.citiesDao().updateCity(city1);
+                    Toast.makeText(requireActivity().getApplicationContext(),"City Updated",Toast.LENGTH_LONG).show();
+                }catch (Exception e){
+                    Toast.makeText(requireActivity().getApplicationContext(),"City already exists",Toast.LENGTH_LONG).show();
+                }
+                cityName.setText("");
+                country.setSelection(0);
+                cityAdapter.clear();
+                citiesList.clear();
+                citiesList.addAll(db.citiesDao().getCityNames());
+                citiesList.add(0, defaultSelection);
+                cityAdapter.addAll(citiesList);
+                city.setSelection(0);
+
+
+            }else
+                Toast.makeText(requireActivity().getApplicationContext(),"Required Fields cannot be empty",Toast.LENGTH_LONG).show();
+        });
     }
 }
